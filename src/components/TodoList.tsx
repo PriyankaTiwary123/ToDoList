68d1b7313252c545fe82258eb900d19ea32cd4de
Priyanka Tiwary <tiwary.priyanka92@gmail.com>
2020-05-25 20:43:15 +0800

    final commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,15 @@
 import * as React from 'react';
 import CardComponent from '../utility/CardComponent'
-import { Button } from '@material-ui/core';
 
 export const TodoList = () => {
-    const [cards, setCards] = React.useState(false);
-    const [cardsArray, setCardsArray] = React.useState([] as any);
-
-
-    const addMoreTaskList = () => {
-        setCards(true);
-
-
-    }
 
     return (
         <div className="container">
-            {console.log("in render")}
             <header>
-                <p>To Do List</p><p><Button variant="contained" color="primary" onClick={addMoreTaskList}>Add ToDolist</Button></p>
+                <h4>To Do List</h4>
             </header>
             <div className="todo-container">
-                    <CardComponent cards={cards} />
+                    <CardComponent/>
 
             </div>
         </div>
--- src/utility/CardComponent.tsx
+++ src/utility/CardComponent.tsx
@@ -9,40 +9,39 @@ export default function CardComponent(props: any) {
     const [cardCount, setCardCount] = React.useState(1);
     const [taskArray, setTaskListArr] = React.useState([] as any);
     const [addCard, setAddCard] = React.useState(false);
-    const [editText, setEditedText] = React.useState("");
-    // const [markDone ,setMarkAsDone]= React.useState(false);
-    const [btnName, setBtnName] = React.useState("");
 
     const setTask = (e: any) => {
         setTaskList(e.target.value);
-        console.log(e.target.value)
     }
 
     const saveTask = () => {
         const taskObject: any = { task: task, markAsDone: false, edited: false }
-        console.log(taskObject)
-        console.log("priyanka", taskArray)
-        // let editedIndex=taskArray.findIndex((x:any)=>x.edited===true);
-        // console.log(editedIndex)
-        // if(editedIndex!==-1){
-        //    taskArray.splice(editedIndex,1)
-        //    console.log(taskArray)
-        //     setTaskListArr(taskArray)
-        // }
-        setTaskListArr([...taskArray, taskObject]);
+        let editedTask = taskArray.filter((x: any) => x.edited === true);
+        let tempTaskArray = [...taskArray];
+        if (editedTask && editedTask.length > 0) {
+            tempTaskArray.splice(tempTaskArray.indexOf(editedTask[0]), 1)
+        }
+        setTaskListArr([...tempTaskArray, taskObject]);
         localStorage.setItem('taskArray', JSON.stringify(taskArray))
-        // console.log(localStorage)
-        console.log(taskArray);
         setAddCard(true)
         setCardCount(cardCount + 1);
-        console.log(cardCount)
     }
+
+    React.useEffect(() => {
+        setTaskListArr(getLocalStorageValue())
+    }, [])
+
+    const getLocalStorageValue = () => {
+        return JSON.parse(localStorage.getItem('taskArray') || '{}')
+    }
+
 
     const addAnotherCard = () => {
         setCardCount(cardCount - 1);
         setAddCard(false);
         setTaskList("")
     }
+    
     const cancelCardAddition = () => {
         setAddCard(true);
     }
@@ -53,39 +52,32 @@ export default function CardComponent(props: any) {
     }
 
     const markAsDone = (element: any, index: number) => {
-        console.log(element, index)
-        let tempObj = Object.assign({}, element)
-        tempObj.markAsDone = true;
-        let newTaskArray = taskArray.splice(index, 1, tempObj)
-        setTaskListArr([...newTaskArray, tempObj]);
-
+        let tempTaskArray = [...taskArray];
+        tempTaskArray[index].markAsDone=true;
+        setTaskListArr(tempTaskArray);
     }
 
     const editTaskList = (element: any, index: number) => {
         setCardCount(cardCount - 1);
         setAddCard(false);
         setTaskList(element.task);
-
-        taskArray.splice(index, 1, element);
-        let tempObjEdited = Object.assign({}, element)
-        tempObjEdited.edited = true;
-        // console.log("cggghgjgjghj",newEditedArray)
-        setTaskListArr([...taskArray, tempObjEdited])
-        setEditedText("edited")
-        console.log("aaaa", taskArray)
+        let tempTaskArray = [...taskArray];
+        tempTaskArray[index].edited=true;
+        setTaskListArr(tempTaskArray);
     }
 
     return (
         <div className="task-conatiner col col-sm-6 col col-xs-12 col col-md-6">
-            {cardCount > 1 || addCard ? taskArray && taskArray.map((element: any, index: number) => {
+            {taskArray && taskArray.map((element: any, index: number) => {
                 return (<div className={element.markAsDone ? "card  added-card-section-" + index : "card  added-card-section"} key={index}><div className="card-body"><p>{element.task}</p>
-                    <div className="task-list-btns">
-                        <EditIcon onClick={() => editTaskList(element, index)}></EditIcon>
-                        <DeleteIcon onClick={() => deleteCard(index)}></DeleteIcon>
-                        <button type="button" className="btn btn-outline-primary" onClick={() => markAsDone(element, index)}>Mark Done</button>
+                    <div className="task-list-btns row">
+                        <EditIcon  className="col col-3" onClick={() => editTaskList(element, index)}></EditIcon>
+                        <DeleteIcon  className="col col-3"onClick={() => deleteCard(index)}></DeleteIcon>
+                        <button type="button" className="btn btn-outline-primary col col-3" onClick={() => markAsDone(element, index)}>Mark Done</button>
                     </div>
                 </div></div>)
-            }) :
+            })}
+            {!addCard ?
                 <div className="card edit-card-section ">
                     <div className="card-body">
                         <input className="task-detail"
@@ -96,7 +88,7 @@ export default function CardComponent(props: any) {
                         />
                     </div>
 
-                </div>}
+                </div> : ''}
             <div className="todo-btns">
                 {addCard ?
                     <button type="button" className="btn btn-secondary" onClick={addAnotherCard}>Add Another Card</button> :
